d29ccf5d00ec433e1c1af0df3a900c15c3e51527
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-03-07 19:28:17 -0400

    week01/csharp-3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guessNumber = 0;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        //Console.Write("What is the magic number?: ");
+        //string userNumber = Console.ReadLine();
+        //magicNumber = int.Parse(userNumber);
+
+        while (magicNumber != guessNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guessNumber = int.Parse(userGuess);
+
+            if (magicNumber < guessNumber) Console.WriteLine("Lower");
+            else Console.WriteLine("Higher");
+        }
+        Console.Write("You guessed it!");
     }
 }
