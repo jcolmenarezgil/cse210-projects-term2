bda6f93a30197aa4870f60413fbd2d3b60b29d7f
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-03-08 13:42:30 -0400

    week01/csharp-4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // using System.Collections.Generic;
+
+        int endNumber = 0;
+        int addNumber;
+        int sum = 0;
+        int LargestNumber = int.MinValue;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> List = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string userNumber = Console.ReadLine();
+            addNumber = int.Parse(userNumber);
+
+            if (addNumber == endNumber)
+            {
+                break;
+            }
+
+            List.Add(addNumber);
+            sum += addNumber;
+
+            if (addNumber > LargestNumber)
+            {
+                LargestNumber = addNumber;
+            }
+        }
+        if (List.Count > 0)
+        {
+            double average = (double)sum / List.Count;
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {LargestNumber}");
+        }
     }
 }
