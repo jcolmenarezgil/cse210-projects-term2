8953054a726341b454caed344d58a45b087f33dd
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-04-11 22:58:01 -0400

    Week 06 Update: Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Blue", 5.0));
+        shapes.Add(new Rectangle("Green", 4.0, 9.0));
+        shapes.Add(new Square("Yellow", 30.0));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}, Area: {s.GetArea()}");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,25 @@
+public abstract class Shape
+{
+    public string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor() {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+/*     public virtual double GetArea()
+    {
+        return 0.0;
+    } */
+
+    public abstract double GetArea();
+}
